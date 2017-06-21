221e91336147ced858fc9296d8a4c3adadb23cfe
Jian Liang <lj299792458@gmail.com>
2017-06-21 17:42:32 -0400

    bug bug bug

--- Var.py
+++ Var.py
@@ -11,9 +11,9 @@ class Var(np.ndarray):
         numberOfBins            = None, # binning
         binningLogicSystem      = None, # binning
         shape                   = None,
-        index                   = tuple(''),
         filename                = '',
-        endian                  = '<'
+        endian                  = '<',
+        data                    = None
         ):
         HeadType = np.dtype([
             ('head', [
@@ -38,18 +38,29 @@ class Var(np.ndarray):
 
         self = np.zeros(shape=shape).view(cls)
 
+        if data is not None:
+            self = np.array(data).view(cls)
+
         if filename != '':
-            head_data = np.fromfile(filename, dtype=HeadType, count=1)[0]
-            n_dims = head_data['head']['n_dims']
-            type = head_data['head']['one_dim']['type'][0:n_dims]
-            typename = [typename[i] for i in type]
-            n_indices = tuple(head_data['head']['one_dim']['n_indices'][0:n_dims])
-            indices = head_data['head']['one_dim']['indices'][0:n_dims]
-            DataType = np.dtype([
+            head_data_ = np.fromfile(filename, dtype=HeadType, count=1)[0]
+            n_dims_ = head_data_['head']['n_dims']
+            type_ = head_data_['head']['one_dim']['type'][0:n_dims_]
+            typename_ = [typename[i] for i in type_]
+            n_indices_ = tuple(head_data_['head']['one_dim']['n_indices'][0:n_dims_])
+            indices_ = head_data_['head']['one_dim']['indices'][0:n_dims_]
+            DataType_ = np.dtype([
             ('head_tmp','S102400'),
-            ('data',endian+'f8',n_indices)
+            ('data',endian+'f8',n_indices_)
             ])
-            self = np.fromfile(filename, dtype=DataType)[0]['data']
+            self = np.fromfile(filename, dtype=DataType_)[0]['data'].view(cls)
+            self.indices = {}
+            for i in range(n_dims_):
+                self.indices[typename_[i]] = indices_[i][0:n_indices_[i]]
+            self.type = typename_
+            self.index = {}
+            for i in range(n_dims_):
+                self.index[typename_[i]] = i
+
 
         self._name              = name
         self.tree               = tree
@@ -57,14 +68,35 @@ class Var(np.ndarray):
         self.eventWeight        = eventWeight
         self.numberOfBins       = numberOfBins
         self.binningLogicSystem = binningLogicSystem
-        self.index              = index
-
         return self
+
+    def find_name(self, name_=''):
+        if name_ in self.type:
+            return self.index[name_]
+        else:
+            return -1
+
+    def jack(self):
+        index_ = self.find_name("conf")
+        if index_ == -1:
+            print("no index conf")
+            exit(-1)
+        #return jack(self,index_)
 
 d = Var()
 print(d.shape)
-a = Var(shape=(2,2), index=('a', 'b'))
+a = Var(shape=(2,2))
 b = Var(shape=(2,2,2))
 a = b
 print(a.shape)
 
+a = Var(filename="rbc_conf_2464_m0.005_0.04_000495_hyp.2pt.dov.glue.data")
+print(a.shape)
+print(a.indices['momentum'])
+print(a.indices[a.type[a.index['momentum']]])
+print(a.type)
+print(a.index['momentum'])
+print(a.find_name(name_='momentum'))
+print(a[0,0,0,0,1,0,0])
+print(a.flat[0])
+print(a.flat[-1])
